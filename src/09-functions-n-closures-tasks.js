4454712c2e26894b9e138b5941efe09564a2b695
Ilja Gusiatin <igusiatin89@gmail.com>
2021-06-21 11:57:01 +0300

    feat: add partialUsingArguments function

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -181,8 +181,9 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const partianFunc = (...args2) => fn(...args1, ...args2);
+  return partianFunc;
 }
 
 
